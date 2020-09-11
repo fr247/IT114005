7d8edc9c426dd02d0bd0772e8eeb73f0adc445a8
fr247 <fr247@njit.edu>
2020-09-11 08:27:52 -0400

    Uploading Java Data Homework

---
+++ Java Data Homework/JavaDataHomework.java
@@ -0,0 +1,17 @@
+public class JavaDataHomework {
+	public static void main(String[] args) {
+		// If it matches my sample exactly it gets a 0
+		// 1. Create a variable that holds a name
+      String name = new String("Francisco");
+		// 2. Output the name to the console
+      System.out.println(name);
+		// 3. Create a variable that holds an age (as a whole number)
+      int age = 19;
+		// 4. Output the age to the console
+      System.out.println(age);
+		// 5. Create a variable that holds a decimal
+      double decimal = 3.141592;
+		// 6. Output the variable
+      System.out.println(decimal);      
+	}
+}
