0b2d7a81fdb6abbb0649bc30bcb752cda0c16eae
fr247 <fr247@njit.edu>
2020-10-02 09:33:31 -0400

    Converted recursion to loop

--- Recursion to Loop/Recursion.java
+++ Recursion to Loop/Recursion.java
@@ -1,13 +1,17 @@
 public class Recursion {
   
 	public static int sum(int num) {
-		if (num > 0) {
-			return num + sum(num - 1);
+      int sumValue = num;
+		while (num > 0) {
+         num--;
+			sumValue = sumValue + num;
 		}
-		return 0;
+		if (num == sumValue){sumValue = 0;}
+      return sumValue;
 	}
+   
 
 	public static void main(String[] args) {
-		System.out.println(sum(10));
+		System.out.println(sum(100));
 	}
 }
